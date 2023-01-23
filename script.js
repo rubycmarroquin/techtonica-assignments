68ea7ac6846d9facc56c26bd350aff61f6ea9a7c
rubycmarroquin <rcmarroq21@gmail.com>
2023-01-23 14:44:40 -0500

    convert list items into checkboxes

---
+++ script.js
@@ -0,0 +1,41 @@
+function handleCheckbox(event) {
+    console.log(event)
+}
+
+/**
+ * Adds a checkbox element to a list of ingredients
+ * @param {string} sectionId - the class name of the list we want to add check
+ * boxes to 
+ */
+function initSection(sectionId) {
+    const sectionElement = document.getElementById(sectionId);
+    const sectionList = Array.from(sectionElement.getElementsByTagName('li'));
+    for(let i = 0; i < sectionList.length; i++) {
+        // store the ingredient text 
+        const text = sectionList[i].textContent;
+        // create an input element 
+        let checkbox = document.createElement("input");
+        // create a label input 
+        let label = document.createElement("label");
+        // add the innerHTML text to be the ingredient text 
+        label.innerHTML = text;
+        // add the type attribute of checbox to be a checkbox 
+        checkbox.type = "checkbox";
+        // add the name attribute of checkbox to checkboxes 
+        checkbox.name = "checkboxes";
+        checkbox.onchange = function onCheckboxChange(e) {
+            label.classList.toggle('checked');
+        }
+        sectionList[i].removeChild(sectionList[i].firstChild);
+        sectionList[i].appendChild(checkbox);
+        sectionList[i].appendChild(label)
+        // sectionList[i].innerHTML = `<input type="checkbox" name="name[]" onChange="handleCheckbox()">${text}`;
+
+    }
+}
+
+// Add check boxes to ingredients 
+initSection('cake')
+initSection('milkMixture')
+initSection('whipped')
+
