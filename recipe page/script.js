0b8e2bedf19e51c5231d5f6ce9513caf84fb067e
rubycmarroquin <rcmarroq21@gmail.com>
2023-01-26 20:52:18 -0500

    Added better documentation to script

--- recipe page/script.js
+++ recipe page/script.js
@@ -36,7 +36,7 @@ initSection('whipped');
 /**
  * Adds a button at the bottom of the HTML page 
  */
-async function addButton() {
+async function addButton(text) {
     // grab the div with the classname container
     let div  = document.getElementsByClassName("container");
     // create new div element for button 
@@ -47,7 +47,7 @@ async function addButton() {
     let button = document.createElement("button");
     button.type = "button";
     button.className = "buttons";
-    button.innerHTML = "Press me for Creator";
+    button.innerHTML = text;
     // append to HTML page 
     divsButton.appendChild(button);
     div[0].appendChild(divsButton);
@@ -55,53 +55,111 @@ async function addButton() {
 }
 
 // it adds a button but there is no text?
-addButton();
+addButton("Press me for Creator");
+addButton("Press me for More Recipes");
 
 /**
  * Adds an event listener to every button element in the HTML document
  */
-async function buttonEventListener () {
-    let buttons = document.getElementsByClassName("buttons");
-    for(let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", () => styleButton(buttons[i]));
-    }
+async function buttonEventListener (button, func) {
+    button.addEventListener("click", () => func(button));
 }
 
-buttonEventListener();
+// get button elems 
+let buttons = document.getElementsByClassName("buttons");
+
+// credit recipe 
+buttonEventListener(buttons[0], styleCreditButton);
+
+//similar recipes 
+buttonEventListener(buttons[1], styleSimilarRecipesButton);
 
 /**
  * Changes the styling of the button
  * @param {button} button - button element that was clicked on
  */
-function styleButton (button) {
-    button.style.padding = "100px"
-    button.style.backgroundColor = "teal";
-    addFooter();
+function styleCreditButton (button) {
+    button.style.backgroundColor = "red";
+    createFooter();
 }
 
 /**
- * Adds a footer to the end of the recipe page that includes 
- * the link to the recipe credit
- * 
- * 
- * <!-- Footer-->
- * <div class="footerContainer">
- *   <footer>
- *     <a id="creditLink" href="https://tastesbetterfromscratch.com/tres-leches-cake/">Credit for Recipe</a>
- *   </footer>
- * </div>
-
+ * Changes the styling of the button
+ * @param {button} button - button element that was clicked on
  */
-async function addFooter() {
+function styleSimilarRecipesButton (button) {
+    button.style.backgroundColor = "red";
+    createSecondFooter();
+}
+
+/**
+ * Creates a second footer to the "Press me for More Recipes"
+ * button and is appended to a new div element. 
+ */
+async function createFooter() {
+    // get the body element 
     let body  = document.getElementsByTagName("body");
+    // create div element 
     let div = document.createElement("div");
+    // create footer element 
     let footer = document.createElement("footer");
+    // create <a> element 
     let link = document.createElement("a");
+    // adds an id to the link 
     link.id = "creditLink";
+    // link reference 
     link.href = "https://tastesbetterfromscratch.com/tres-leches-cake/";
+    // add text to the link 
     link.textContent = "Credit for the Recipe";
+    // adds class name to the div element 
     div.className = "footerContainer";
+    // add link to footer 
     footer.appendChild(link);
+    // add footer to div 
     div.appendChild(footer);
+    // add div to body
     body[0].appendChild(div);
+}
+
+/**
+ * Creates a second footer to the "Press me for More Recipes"
+ * button and is appended to a new div element. 
+ */
+async function createSecondFooter() {
+    // get body element 
+    let body  = document.getElementsByTagName("body")[0];
+    // create footer element 
+    let footer = document.createElement("footer");
+    // create div element 
+    let div = document.createElement("div");
+    // add class name to div element 
+    div.className = "footerContainer";
+
+    // create link elements for each recipe 
+    let recipe1 = document.createElement("a");
+    let recipe2 = document.createElement("a");
+    let recipe3 = document.createElement("a");
+
+    // recipe 1: add links and change link name 
+    recipe1.href = "https://tastesbetterfromscratch.com/tres-leches-cake/";
+    recipe1.textContent = "Credit for the Recipe";
+    
+    // recipe 2: add links and change link name
+    recipe2.href = "https://tastesbetterfromscratch.com/tres-leches-cake/";
+    recipe2.textContent = "Credit for the Recipe";
+    
+    // recipe 3: add links and change link name 
+    recipe3.href = "https://tastesbetterfromscratch.com/tres-leches-cake/";
+    recipe3.textContent = "Credit for the Recipe";
+
+    // add recipe elements to the footer element
+    footer.appendChild(recipe1);
+    footer.appendChild(recipe2);
+    footer.appendChild(recipe3);
+    
+    // add footer element to div element 
+    div.appendChild(footer);
+
+    // add div element to the body 
+    body.appendChild(div);
 }
