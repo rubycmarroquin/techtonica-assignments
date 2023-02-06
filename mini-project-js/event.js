40df553b03b6e2d65103d90f90e7a8dbae278b5d
rubycmarroquin <rcmarroq21@gmail.com>
2023-02-06 10:42:05 -0500

    Documented code and reformatted

--- mini-project-js/event.js
+++ mini-project-js/event.js
@@ -1,14 +1,19 @@
+/**
+ * Creates a class called "Event" and defines a constructor that takes in two inputs?
+ * a name of the event and description of the event. 
+ * Constructor also has an array called "available tickets"
+ */
 class Event {
     constructor(name, description) {
-      this.name = name;
-      this.description = description;
-      this.availableTickets = [];
+        this.name = name;
+        this.description = description;
+        this.availableTickets = [];
     }
-  }
+}
 
 // The below statement creates an object.
-const eventObj1 = new Event(
-    'KLOS Golden Gala',
-    'An evening with hollywood vampires'
-  );
-  
+const eventObj1 = new Event('KLOS Golden Gala', 'An evening with hollywood vampires');
+
+// create more objects with diff values 
+const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
+const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
