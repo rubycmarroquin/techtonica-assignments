248f296393f7e0938a17155c74cdf436cd1c12ce
rubycmarroquin <rcmarroq21@gmail.com>
2023-02-06 10:44:45 -0500

    created an event array and push event objs

--- mini-project-js/event.js
+++ mini-project-js/event.js
@@ -17,3 +17,18 @@ const eventObj1 = new Event('KLOS Golden Gala', 'An evening with hollywood vampi
 // create more objects with diff values 
 const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
 const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
+
+// create an empty Event array 
+const eventArray = new Array();
+
+/* Push objects into the array */ 
+
+// pushing single object to an array
+eventArray.push(eventObj1);
+// check to see if eventObj1 got push into array 
+console.log(eventArray);
+
+// pushing multiple objects to an array at once
+eventArray.push(eventObj1, eventObj2, eventObj3);
+// check to see if both objs got pushed into array 
+console.log(eventArray);
