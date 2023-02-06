5516a54d0b1b92445a686adc6c334961dc955d40
rubycmarroquin <rcmarroq21@gmail.com>
2023-02-06 10:37:05 -0500

    created object and assigned values to it

--- mini-project-js/event.js
+++ mini-project-js/event.js
@@ -5,3 +5,10 @@ class Event {
       this.availableTickets = [];
     }
   }
+
+// The below statement creates an object.
+const eventObj1 = new Event(
+    'KLOS Golden Gala',
+    'An evening with hollywood vampires'
+  );
+  
