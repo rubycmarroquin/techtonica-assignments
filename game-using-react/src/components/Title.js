66cf53a347124905936feccc04230a0eabcf99e8
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-02-16 21:42:38 -0500

    Created Title Component

---
+++ game-using-react/src/components/Title.js
@@ -0,0 +1,9 @@
+function Title() {
+    return ( 
+        <div className="titleOfGameDiv">
+        <h1 className="titleOfGame">Guess the <span id="word">Word</span></h1>
+        </div>
+    )
+}
+
+export default Title;
