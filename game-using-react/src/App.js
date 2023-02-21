8b257b4ed2b771391ed1f1e6d53676b8088d1c54
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-02-21 10:39:40 -0500

    reformatted code using prettier

--- game-using-react/src/App.js
+++ game-using-react/src/App.js
@@ -1,45 +1,70 @@
-import {useState} from 'react';
-import Title from './components/Title';
-import GameBoard from './components/GameBoard';
-import './App.css';
+import { useState } from "react";
+import Title from "./components/Title";
+import GameBoard from "./components/GameBoard";
+import "./App.css";
 
 function App() {
-
   /* Handles the startButton being clicked and hidden */
   const [startButtonClicked, setStarted] = useState(false);
-  
+
   const startClicked = () => {
-    // hide the start button 
+    // hide the start button
     setStarted(true);
-  }
+  };
 
   return (
     <div className="mainDiv">
       <Title />
-      {
-        !startButtonClicked ? (      
+      {!startButtonClicked ? (
         <div className="startButtonDiv">
-          <button type="button" className={`startButton ${!startButtonClicked ? "" : "hideStartButton"}`} id="startButton" onClick={startClicked}>
+          <button
+            type="button"
+            className={`startButton ${
+              !startButtonClicked ? "" : "hideStartButton"
+            }`}
+            id="startButton"
+            onClick={startClicked}
+          >
             Start Game
           </button>
         </div>
-      ) : 
-      
-      (      
-      <GameBoard wordList={words}/>
-      )
-    }
-
-
+      ) : (
+        <GameBoard wordList={words} />
+      )}
     </div>
   );
 }
 
 export default App;
 
-// Array of words that we will use for the game 
-const words = ['penguins', 'elephant', 'koala', 'synonym', 'peanut',
-    'english', 'ruby', 'react', 'friday', 'airplane', 'banana', 'birthday',
-    'waterfall', 'trampoline', 'window', 'president', 'basketball', 'hiccup',
-    'cheerleader', 'kangaroo', 'groundhog', 'soccer', 'handball', 'caterpillar',
-    'shoelaces', 'hungry', 'fishing', 'monster'];
+// Array of words that we will use for the game
+const words = [
+  "penguins",
+  "elephant",
+  "koala",
+  "synonym",
+  "peanut",
+  "english",
+  "ruby",
+  "react",
+  "friday",
+  "airplane",
+  "banana",
+  "birthday",
+  "waterfall",
+  "trampoline",
+  "window",
+  "president",
+  "basketball",
+  "hiccup",
+  "cheerleader",
+  "kangaroo",
+  "groundhog",
+  "soccer",
+  "handball",
+  "caterpillar",
+  "shoelaces",
+  "hungry",
+  "fishing",
+  "monster",
+];
