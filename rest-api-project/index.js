1adb9f158ccacb9d99364cfbca352770dd2782a6
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-02-23 11:53:04 -0500

    add(index.js):code to get a simple server running

---
+++ rest-api-project/index.js
@@ -0,0 +1,23 @@
+/**
+ * Code to get a simple server running
+ * source: https://dev.to/miku86/nodejs-how-to-create-a-simple-server-using-express-1n9d#:~:text=Write%20a%20simple%20script&text=%2F%2F%20import%20express%20(after%20npm,res)%20%3D%3E%20%7B%20res.
+ */
+
+// import express (after npm install express)
+const express = require('express');
+
+// create new express app and save it as "app"
+const app = express();
+
+// server configuration
+const PORT = 5001;
+
+// create a route for the app
+app.get('/', (request, response) => {
+  response.send('Hello World');
+});
+
+// make the server listen to requests
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
