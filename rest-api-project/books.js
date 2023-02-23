91827799d6d784948ebb698715abb44bf76ba36d
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-02-23 16:51:55 -0500

    added(books.js): array of book objects

---
+++ rest-api-project/books.js
@@ -0,0 +1,40 @@
+const BOOKS = [
+    {
+        title: "The Hunger Games",
+        author: "Suzanne Collins",
+        isbn: 9780545791878,
+        publisher: "Bloomsbury"
+    },
+    {
+        title: "Fifty Shades of Grey",
+        author: "James, E. L.",
+        isbn: 9780099579939,
+        publisher: "Random House"
+    },
+    {
+        title: "Angels and Demons",
+        author: "Brown, Dan",
+        isbn: 9780552161701,
+        publisher: "Transworld"
+    },
+    {
+        title: "Twilight",
+        author: "Meyer, Stephenie",
+        isbn: 9780316033411,
+        publisher: "Little, Brown Book"
+    },
+    {
+        title: "The Lovely Bones",
+        author: "Sebold, Alice",
+        isbn: 9780307475343,
+        publisher: "Pacman Macmillan"
+    },
+    {
+        title: "The Da Vinci Code",
+        author: "Brown, Dan",
+        isbn: 9780307474278,
+        publisher: "Transworld"
+    },
+];
+
+export default BOOKS;
