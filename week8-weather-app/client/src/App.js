53d655b0fd4290ac189a6991ad68737be9a197ea
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-02 20:41:42 -0800

    app.js: got basic functionality working

--- week8-weather-app/client/src/App.js
+++ week8-weather-app/client/src/App.js
@@ -8,25 +8,36 @@ function App() {
   const [result, setResult] = useState(null);
 
   //A function to do the get request and set the state from the hard code data
-  const loadCity = () => {
-    fetch("http://localhost:8081/api/weather")
+  const loadCity = (city) => {
+    // pass city name as a param 
+    const params = new URLSearchParams({ cityName: city });
+    // fetch the data from the backend 
+    fetch(`http://localhost:8081/weather?${params}`)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result)
+        // console.log("this is the data: ", result)
         setCity(result.weather[0].name);
         setResult(result);
       });
   }
 
- const handleSubmit = (e) =>{
+    const handleCityChange = (event) => {
+    // console.log(event.target.value)
+    // set city value from child 
+    setCity(event.target.value);
+    // console.log(city);
+  }
+
+ const handleSubmit = (e) => {
   e.preventDefault();
-  loadCity();
+  // pass in the city to loadCity to get data from backend 
+  loadCity(city);
  }
 
 
   return (
     <div className="App">
-      <WeatherForm city={city} handleSubmit={handleSubmit}/>
+      <WeatherForm city={city} handleCityChange={handleCityChange} handleSubmit={handleSubmit}/>
       {!result ? <p>Please click the botton to see Data</p> : <WeatherCard data={result} /> }
     </div>
   );
