0ba235118af63112e8abce26114ab8ee306e8d41
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-02 19:32:27 -0800

    deleted(server.js): removed reference to hardcoded data

--- week8-weather-app/server/server.js
+++ week8-weather-app/server/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const dataWeather = require("./data")
 
 const app = express();
 
-const PORT = 8080;
+const PORT = 8081;
 app.use(cors());
 app.use(express.json());
 
