49145b6e3286a89b42e834a8d09d7592976b67ac
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-02 19:27:29 -0800

    added(server.js): connect open weather api

---
+++ week8-weather-app/server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const dataWeather = require("./data")
+
+const app = express();
+
+const PORT = 8080;
+app.use(cors());
+app.use(express.json());
+
+// creates an endpoint for the route /api
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from My template ExpressJS' });
+  });
+
+// creates an endpoint for the route /api/weather
+app.get('/api/weather', (req, res) => {
+  res.json(dataWeather);
+});
+
+// connects to the open weather api 
+app.get("/weather", (request, response) => {
+  const city = req.query.cityName;
+  const apiKey = process.env.API_KEY;
+  const params = new URLSearchParams({
+    q: req.querycityName,
+    appid: process.env.API_KEY,
+    units: "imperial",
+  });
+  const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
+  console.log(url);
+  fetch(url).then((res) => res.json()).then((data) => {
+    res.send({data});
+  }).catch((error) => {console.log(error)});
+});
+
+// console.log that your server is up and running
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
