cafcd9404480ba0f668b6cef7e68e9e19afae8c6
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-03 14:43:48 -0800

    added(weatherCard.js): more data from response

--- week8-weather-app/client/src/components/weatherCard.js
+++ week8-weather-app/client/src/components/weatherCard.js
@@ -1,31 +1,73 @@
-const WeatherCard = ({data}) => {
-    return (
-        <div className="weather-card">
-            <div className="result">
-                <p>City: <span className="data">
-                    {data.data.name}, {data.data.sys.country}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  import { faWind } from '@fortawesome/free-solid-svg-icons';
+  import { faDroplet } from '@fortawesome/free-solid-svg-icons'
+const WeatherCard = ({ data }) => {
+  return (
+    <div className="weatherCard">
+      <div className="result">
+        {
+          // when city is not valid
+          data.data.cod === "404" ? (
+            <h2>City Not Found</h2>
+          ) : (
+            <>
+              <p className="nameOfCity">
+                {data.data.name}, {data.data.sys.country}
+              </p>
+              <p>
+                Current temperature:{" "}
+                <span>
+                  <strong>
+                    {Math.round(data.data.main.temp)} <sup>o</sup>F
+                  </strong>
                 </span>
-                </p>
-                <p>Description: <span className="data">
-                {data.data.weather[0].description}
-              </span>
-            </p>
-            <img src={`http://openweathermap.org/img/wn/${data.data.weather[0].icon}@4x.png`} alt={"Icon from Open Weather Api"}/>
-            <p>
-              Temperature: <span className="data">
-                {data.data.main.temp} <sup>o</sup>F
-              </span>
-            </p>
-            <p>
-              Feels Like: <span className="data">
-                {data.data.main.feels_like} <sup>o</sup>F
-              </span>
-            </p>
-
-            </div>
-
-        </div>
-    )
-}
+              </p>
+              <p>
+                Feels Like{" "}
+                <span>
+                  <strong>
+                    {Math.round(data.data.main.feels_like)} <sup>o</sup>F
+                  </strong>
+                </span>
+              </p>
+              <p>
+                Max:{" "}
+                <strong>
+                  {Math.round(data.data.main.temp_max)}
+                  <sup>o</sup>F
+                </strong>{" "}
+                Min:{" "}
+                <strong>
+                  {Math.round(data.data.main.temp_min)}
+                  <sup>o</sup>F
+                </strong>
+              </p>
+              <p>
+                Wind: <FontAwesomeIcon icon={faWind} /> {Math.round(data.data.wind.speed)}mph
+              </p>
+              <p>
+                Humidity: <FontAwesomeIcon icon={faDroplet} /> {Math.round(data.data.wind.speed)}%
+              </p>
+              <div className="weatherIconDiv">
+                <div id="description">
+                  <p className="description">
+                    {data.data.weather[0].description}
+                  </p>
+                </div>
+                <div id="icon">
+                  <img
+                    className="weatherIcon"
+                    src={`http://openweathermap.org/img/wn/${data.data.weather[0].icon}@4x.png`}
+                    alt={"Icon from Open Weather Api"}
+                  />
+                </div>
+              </div>
+            </>
+          )
+        }
+      </div>
+    </div>
+  );
+};
 
 export default WeatherCard;
