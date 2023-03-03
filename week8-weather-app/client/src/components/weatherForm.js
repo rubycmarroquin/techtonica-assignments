02e73cfd1785b60c2a4ecac7d4e4e5282e36bf41
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-02 20:44:42 -0800

    changed(weatherForm.js): implementation, now uses props

--- week8-weather-app/client/src/components/weatherForm.js
+++ week8-weather-app/client/src/components/weatherForm.js
@@ -1,12 +1,5 @@
-import { useState } from "react";
 const WeatherForm = (props) => {
-
-  const [cityName, setCityName] = useState("");
-  const handleCityChange = (event) => {
-    setCityName(event.target.value);
-    console.log(cityName);
-  }
-
+  //what is inside props = {city, handleCityChange, handleSubmit}
     return (
         <div className="weather">
         <h1 className="App-header">Weather Forecast</h1>
@@ -16,8 +9,10 @@ const WeatherForm = (props) => {
             type="text"
             placeholder="Please enter your city"
             name="city"
-            value={cityName}
-            onChange={handleCityChange} // set the cityName 
+            // value={cityName}
+            // onChange={handleCityChange} // set the cityName 
+            value={props.city}
+            onChange={props.handleCityChange}
             required // instead of readonly, change to required
           />
           <input type="submit" value="Submit" />
