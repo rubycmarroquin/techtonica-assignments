b5a2b679a8cce2536ca319ab2419ff565d1824b6
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-07 07:58:04 -0800

    added(server.js): get method to connect to trivia api

--- trivia-app/server/server.js
+++ trivia-app/server/server.js
@@ -9,6 +9,29 @@ app.get("/", (req, response) => {
   response.send("I am working!");
 });
 
+//connects to the trivia api 
+app.get("/game", (req, res) => {
+  const amount = req.query.amount;
+  const category = req.query.category;
+  const difficulty = req.query.difficulty;
+  const params = new URLSearchParams({
+    amount: amount,
+    category: category,
+    difficulty: difficulty
+  });
+  const url = `https://opentdb.com/api.php?${params}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      res.send({ data });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
