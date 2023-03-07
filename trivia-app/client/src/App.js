bb0b123cd6c7f644207257d91b9b983457a76318
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-07 14:14:47 -0800

    added(App.js): functionality to api fetch

--- trivia-app/client/src/App.js
+++ trivia-app/client/src/App.js
@@ -1,29 +1,52 @@
-import './App.css';
-import {useState} from 'react';
+import "./App.css";
+import { useState } from "react";
+import StartMenu from "./components/StartMenu";
 
 function App() {
-  
   const [data, setData] = useState(null);
-  
+  const [started, setStarted] = useState(false);
+  const [amount, setAmount] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [difficulty, setDifficulty] = useState(null);
+
   const loadQuestions = () => {
-    const params = new URLSearchParams({ amount: 10, category: 23, difficulty: 'easy' });
-    // fetch the data from the backend 
+    const params = new URLSearchParams({
+      amount: amount,
+      category: category,
+      difficulty: difficulty,
+    });
+    // fetch the data from the backend
     fetch(`http://localhost:3001/game?${params}`)
       .then((response) => response.json())
       .then((result) => {
-        console.log("this is the data: ", result)
+        console.log("this is the data: ", result);
         setData(result);
       });
-  }
-  const handleButton= () => {
-    loadQuestions();
-  }
+
+    setStarted(true);
+  };
 
   return (
     <div>
-      <h1>Hello World</h1>
-      <button onClick={handleButton}>Press me for the data</button>
-      <h2></h2>
+      <h1>Welcome to The Trivia Game</h1>
+      {!started ? (
+        <>
+          <StartMenu
+            setAmount={setAmount}
+            setCategory={setCategory}
+            setDifficulty={setDifficulty}
+          />
+          <button
+            type="button"
+            onClick={loadQuestions}
+            disabled={!amount || !category || !difficulty}
+          >
+            Start Game
+          </button>
+        </>
+      ) : (
+        <h2> Started Game</h2>
+      )}
     </div>
   );
 }
