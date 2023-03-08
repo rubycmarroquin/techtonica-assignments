1f9ca1a90ac1533fc78e4995acc9d0b9f512e7aa
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-07 21:10:06 -0800

    Added(GameScreen.js): condition to render questions or end game

--- trivia-app/client/src/components/GameScreen.js
+++ trivia-app/client/src/components/GameScreen.js
@@ -1,20 +1,34 @@
 import Question from "./Question";
 import { useState } from "react";
-// score tracker 
+// score tracker
 // check whether the user has answered all questions
-    // end game if they have 
-    // continue if they havent 
-// want to pass in the data to the question comp 
-    // in comp want to increment score if user got the question right 
+// end game if they have
+// continue if they havent
+// want to pass in the data to the question comp
+// in comp want to increment score if user got the question right
 
-const GameScreen = ({data}) => {
-    const [score, setScore] = useState(0);
-    return (
-        <div>
-            <Question result={data.data.results[0]} setScore={setScore} score={score}/>
-            <h1>Your score is: {score}</h1>
-        </div>
-    )
-}
+const GameScreen = ({ data }) => {
+  const [score, setScore] = useState(0);
+  const [questionNum, setQuestionNumber] = useState(0);
+
+  return (
+    <div>
+      {questionNum !== data.data.results.length ? (
+        <>
+          <Question
+            result={data.data.results[questionNum]}
+            score={score}
+            setScore={setScore}
+            questionNum={questionNum}
+            setQuestionNumber={setQuestionNumber}
+          />
+        </>
+      ) : (
+        <h1>Game Over</h1>
+      )}
+    <h1>Your score is: {score}</h1>{" "}
+    </div>
+  );
+};
 
 export default GameScreen;
