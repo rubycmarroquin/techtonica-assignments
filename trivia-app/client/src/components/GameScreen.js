b0f98c89359d0cab435fcac603fe5d731e4ac8e1
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-10 08:34:30 -0800

    added(GameScreen.js) end screen & reformatted file

--- trivia-app/client/src/components/GameScreen.js
+++ trivia-app/client/src/components/GameScreen.js
@@ -1,7 +1,15 @@
 import Question from "./Question";
+import EndScreen from "./EndScreen";
+import Score from "./Score";
 import { useState } from "react";
 
-const GameScreen = ({ data, setStarted, setAmount, setCategory, setDifficulty }) => {
+const GameScreen = ({
+  data,
+  setStarted,
+  setAmount,
+  setCategory,
+  setDifficulty,
+}) => {
   const [score, setScore] = useState(0);
   console.log(data);
   const [questionNum, setQuestionNumber] = useState(0);
@@ -10,7 +18,7 @@ const GameScreen = ({ data, setStarted, setAmount, setCategory, setDifficulty })
     setAmount(null);
     setCategory(null);
     setDifficulty(null);
-  }
+  };
 
   return (
     <div className="GameScreenDiv">
@@ -24,21 +32,14 @@ const GameScreen = ({ data, setStarted, setAmount, setCategory, setDifficulty })
             questionNum={questionNum}
             setQuestionNumber={setQuestionNumber}
           />
-          <div className="ScoreDiv">
-            <h2>Current Score: {score}</h2>{" "}
-          </div>
+          <Score score={score} />
         </>
       ) : (
-        <div className="EndGameScreen">
-          <h1 className="GameOverText">Game Over</h1>
-          <div>
-            <h2>Your final score: {score}/{questionNum}</h2>
-          </div>
-          <div>
-            <h3>Want to play again?</h3>
-            <button className="PlayAgain" type="button" onClick={resetGame}>Play Again</button>
-          </div>
-        </div>
+        <EndScreen
+          score={score}
+          resetGame={resetGame}
+          questionNum={questionNum}
+        />
       )}
     </div>
   );
