bad827b357900559322797abbc19c9c7cbe2243e
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-10 12:19:00 -0800

    changed(Questions.js): broke into more components

--- trivia-app/client/src/components/Question.js
+++ trivia-app/client/src/components/Question.js
@@ -1,3 +1,4 @@
+import AnswerChoices from "./AnswerChoices";
 const Question = ({
   amount,
   result,
@@ -16,7 +17,6 @@ const Question = ({
     txt.innerHTML = str;
     return txt.value;
   }
-  
   // decode question from results
   const question = decode(result.question);
 
@@ -33,7 +33,6 @@ const Question = ({
   const shuffledAnswerOptions = choices.sort((a, b) => 0.5 - Math.random());
 
   const handleUserChoice = (e) => {
-    console.log(e.target.value);
     /* comparing user answer to decoded correct answer */
     if (shuffledAnswerOptions[e.target.value] === correct_answer)
       setScore(score + 1);
@@ -47,77 +46,17 @@ const Question = ({
         Question {questionNum + 1}/{amount}:
       </h2>
       <p>{question}</p>
-      {result.type === "multiple" ? (
-        <div className="QuestionChoices">
-          <form>
-            <div className="RadioButton">
-              <input
-                id="option1"
-                type="radio"
-                name="answer_choice"
-                value="0"
-                onChange={handleUserChoice}
-              />
-              <label htmlFor="option1">{shuffledAnswerOptions[0]}</label>
-            </div>
-            <div className="RadioButton">
-              <input
-                id="option2"
-                type="radio"
-                name="answer_choice"
-                value="1"
-                onChange={handleUserChoice}
-              />
-              <label htmlFor="option2">{shuffledAnswerOptions[1]}</label>
-            </div>
-            <div className="RadioButton">
-              <input
-                id="option3"
-                type="radio"
-                name="answer_choice"
-                value="2"
-                onChange={handleUserChoice}
-              />
-              <label htmlFor="option3">{shuffledAnswerOptions[2]}</label>
-            </div>
-            <div className="RadioButton">
-              <input
-                id="option4"
-                type="radio"
-                name="answer_choice"
-                value="3"
-                onChange={handleUserChoice}
-              />
-              <label htmlFor="option4">{shuffledAnswerOptions[3]}</label>
-            </div>
-          </form>
-        </div>
-      ) : (
-        <div>
-          <form>
-            <div className="RadioButton">
-              <input
-                type="radio"
-                id="option1"
-                name="answer_choice"
-                value="0"
-                onChange={handleUserChoice}
-              />
-              <label htmlFor="option1">{shuffledAnswerOptions[0]}</label>
-            </div>
-            <div className="RadioButton">
-              <input
-                id="option2"
-                type="radio"
-                name="answer_choice"
-                value="1"
-                onChange={handleUserChoice}
-              />
-              <label htmlFor="option2">{shuffledAnswerOptions[1]}</label>
-            </div>
-          </form>
-        </div>
-      )}
+      <div className="QuestionChoices">
+        <form>
+          {shuffledAnswerOptions.map((option, index) => (
+            <AnswerChoices
+              option={option}
+              index={index}
+              handlerUserChoice={handleUserChoice}
+            />
+          ))}
+        </form>
+      </div>
     </div>
   );
 };
